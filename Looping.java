1a684f4c0c11288134a44d0ea2e4bf295f67ce1f
umamucheli <110252392+umamucheli@users.noreply.github.com>
2022-07-30 07:12:47 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,72 @@
+public class Looping
+{
+ public static void main(String args[])
+ {
+  int n=5;
+  for(int i=0;i<n;i++) 
+  {
+   for(int j=0;j<n;j++)
+   {
+     if(i==0||i==n-1||j==(n-1)/2)
+        System.out.print("*");
+     else
+        System.out.print(" ");
+    }
+      System.out.print(" ");
+     
+   for(int j=0;j<n;j++)
+   {
+     if(j==0||j==n-1||i==j)
+       System.out.print("*");
+     else
+       System.out.print(" ");
+   }
+    System.out.print(" ");
+   for(int j=0;j<n;j++)
+   {
+     if(i==0||i==(n-1)/2||i==n-1||j==0)
+       System.out.print("*");
+     else
+      System.out.print(" ");
+   }
+     System.out.print(" ");
+    for(int j=0;j<n;j++)
+    {
+      if(j==0||j==n-1||i==n-1)
+         System.out.print("*");
+      else
+         System.out.print(" ");
+    }
+      System.out.print(" ");
+    for(int j=0;j<n;j++)
+    {
+      if(i==0&&i!=n-1||j==0||i==(n-1)/2||i==j&&i>=(n-1)/2||i>0&&i<(n-1)/2&&j==n-1)
+         System.out.print("*");
+      else
+         System.out.print(" ");
+    }
+      System.out.print(" ");
+     for(int j=0;j<n;j++)
+     {
+       if(i==0&&j!=0&&j!=n-1||i==n-1&&j!=0&&j!=n-1||j==0&&i!=0&&i!=n-1||j==n-1&&i!=0&&i!=n-1)
+           System.out.print("*");
+       else
+           System.out.print(" ");
+     }
+       System.out.print(" ");
+      for(int j=0;j<n;j++)
+      {
+        if(j==0||j==n-1||i==j)
+          System.out.print("*");
+        else
+          System.out.print(" ");
+      }
+        System.out.println(" ");
+   }
+  }
+ }
+        
+
+
+
+   
